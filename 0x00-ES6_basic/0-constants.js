8ec98a10961b4f89194cb489deca82de5c885dff
mohamedbaker <dam.vampire22@gmail.com>
2024-06-03 04:01:37 +0300

    es lint fix

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,12 +1,12 @@
 export const taskFirst = () => 'I prefer const when I can.';
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export let taskNext =() => {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+
+export function getLast() {
+  return ' is okay';
+}
+
+export const taskNext = () => {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+};
