6bd31cfad2ac71f984718b7f16097e56e921c5bb
mohamedbaker <dam.vampire22@gmail.com>
2024-08-21 05:21:37 +0300

    dispaly msg and READme

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * displayMessage: prints in STDOUT the string argument.
+ * msg: The message to display.
+ */
+const displayMessage = (message) => {
+    console.log(message);
+}
+
+module.exports = displayMessage
